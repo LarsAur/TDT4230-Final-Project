1afc0469be7d8c50409b26be4a2a3fc4abd8506f
larsaur <lars.m.aurud@gmail.com>
2022-03-10 01:29:42 +0100

    First triangle

--- makefile
+++ makefile
@@ -1,5 +1,13 @@
+
+.PHONY: run build clean
+
 run:
 	cd ./build && make && PortalProject.exe
 
+build:
+	cmake -G "MinGW Makefiles" -B ./build -S .
+
 clean:
-	
+	-del /q /s build\*
+	-rmdir /q /s build\*
+	-mkdir build && cd build && copy NUL .gitkeep
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,80 @@
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
 #include <iostream>
-using namespace std;
+
+#include <shader.hpp>
+
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f, // left
+    0.5f, -0.5f, 0.0f,  // right
+    0.0f, 0.5f, 0.0f    // top
+};
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 
 int main(int argc, char *argv[])
 {
-#ifdef PROJECT_SOURCE_DIR
-    cout << PROJECT_SOURCE_DIR << endl;
-#endif
 
-    cout << "Hello World" << endl;
-    return 0;    
+    if (!glfwInit())
+    {
+        std::cout << "Failed to init GLFW" << std::endl;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(1200, 800, "Portal", NULL, NULL);
+    if (!window)
+    {
+        std::cout << "Failed to initialize window" << std::endl;
+        glfwTerminate();
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    glfwSwapInterval(1);
+
+    Shader shader;
+    shader.attach("../shaders/test.vert");
+    shader.attach("../shaders/test.frag");
+    shader.link();
+    shader.activate();
+
+    unsigned int vao, vbo;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    int positionLocation = glGetAttribLocation(shader.getProgramID(), "position");
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        glViewport(0, 0, width, height);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }
